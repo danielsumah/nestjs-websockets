574d2272beb7a0bb55d2f5956d82afb1560d2d94
danielsumah <danielsumah@gmail.com>
2025-06-09 10:56:48 +0100

    emitting to client vs emitting to server

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,10 +1,14 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
-import { Socket } from 'socket.io'
+import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { Server, Socket } from 'socket.io'
 @WebSocketGateway(3003, {})
 export class ChatGateWay {
+    @WebSocketServer() server: Server
+
     @SubscribeMessage('events')
     handleEvent(client: Socket, message: any) {
         console.log({ message })
         client.emit('response', `You sent: ${message}`)
+        this.server.emit('response', `Someone sent: ${message}`)
+
     }
 }
