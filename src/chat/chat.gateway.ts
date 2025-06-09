f84431d584addaf625d11a2c31a36afa1a7ab1eb
danielsumah <danielsumah@gmail.com>
2025-06-09 11:13:40 +0100

    handling connections and disconnections

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,10 +1,22 @@
-import { MessageBody, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
+import { MessageBody, OnGatewayConnection, OnGatewayDisconnect, SubscribeMessage, WebSocketGateway, WebSocketServer } from "@nestjs/websockets";
 import { Server, Socket } from 'socket.io'
 @WebSocketGateway(3003, {})
-export class ChatGateWay {
+export class ChatGateWay implements OnGatewayConnection, OnGatewayDisconnect{
     @WebSocketServer() server: Server
 
-    @SubscribeMessage('events')
+    handleConnection(client: Socket, ...args: any[]) {
+        this.server.emit("user-joined", {
+            message: `${client.id} joined the group`
+        })
+    }
+
+    handleDisconnect(client: Socket) {
+        this.server.emit("user-left", {
+            message: `${client.id} left the group`
+        })
+    }
+
+    @SubscribeMessage('new-message')
     handleEvent(client: Socket, message: any) {
         console.log({ message })
         client.emit('response', `You sent: ${message}`)
