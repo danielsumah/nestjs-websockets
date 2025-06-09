35b70b33083b6bbcafcbce09bcee18f55e221234
danielsumah <danielsumah@gmail.com>
2025-06-09 10:39:12 +0100

    emitting response

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,10 +1,10 @@
 import { MessageBody, SubscribeMessage, WebSocketGateway } from "@nestjs/websockets";
-
+import { Socket } from 'socket.io'
 @WebSocketGateway(3003, {})
 export class ChatGateWay {
     @SubscribeMessage('events')
-    handleEvent(@MessageBody() data: any): string {
-        console.log(data)
-        return data;
+    handleEvent(client: Socket, message: any) {
+        console.log({ message })
+        client.emit('response', `You sent: ${message}`)
     }
 }
